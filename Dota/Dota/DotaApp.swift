a9f5899d322ef881eb06e7731edf1a12d1b98671
Casprine Assempah <casprine.001@gmail.com>
2021-06-14 09:01:33 +0000

    added base dota app

---
+++ Dota/Dota/DotaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotaApp.swift
+//  Dota
+//
+//  Created by Cas on 14/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct DotaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
